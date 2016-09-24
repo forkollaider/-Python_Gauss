f6563842c258f6ddfd47b2f191119041475a70aa
aliot2010 <aliot2010@tut.by>
2016-09-24 13:32:45 +0300

    Финальный коммит тестовой ветки

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
-import gauss
 import numpy
 
-a = numpy.array([[1., 2, 1, 1],
-                [1, 2 , 1, 1],
-                [0, 1, 1, 2]])
-#a= numpy.array([[ 1. ,  0.6, -0.4,  0.4],
-               # [ 0.,   1.,  -1.5,  1.5],
-               # [ 0.,   0.,   1.,   1. ]])
+import gauss
+
+
+a = numpy.array([[2., 2, 1, 1],
+                 [1, 2, 1, 1],
+                 [0, 1, 1, 2]])
 print(a)
-#print("\n")
+print("\n")
 
 b = gauss.gaussFunc(a)
-#print("Ответ:")
+print("Ответ:")
 print(b)
